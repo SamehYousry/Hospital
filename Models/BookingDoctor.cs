ca4b1c1b1da76e2917305c184663ce47ab178c70
Sameh Yousry <81245601+SamehYousry@users.noreply.github.com>
2023-04-29 01:52:53 +0200

    Add files via upload

---
+++ Models/BookingDoctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalManegement.Models
+{
+    public class BookingDoctor
+    {
+
+
+        public string doctorid { set; get; }
+        public Doctor doctor { set; get; }
+        [Required]
+        public string patientid { set; get; }
+        [NotMapped]
+        public Patient patient { set; get; }
+        public DateTime reservedate { set; get; }
+
+        public string prescription { set; get; }
+
+
+        public string ComplainDetailsID { set; get; }
+        public ComplainDetails ComplainDetails { set; get; }
+
+    }
+}
---
+++ Models/ComplainDetails.cs
@@ -0,0 +1,23 @@
+﻿namespace HospitalManegement.Models
+{
+    public class ComplainDetails
+    {
+
+        public string id { get; set; }
+        public string DOB { get; set; }
+        public string ChiefComplaint { get; set; }
+        public bool b1 { get; set; }
+        public bool b2 { get; set; }
+        public bool b3 { get; set; }
+        public bool b4 { get; set; }
+        public bool b5 { get; set; }
+        public bool b6 { get; set; }
+        public bool b7 { get; set; }
+        public bool b8 { get; set; }
+        public string AdditionalInformations { get; set; }
+
+        public string BookingDoctorID { set; get; }
+        public BookingDoctor BookingDoctor { set; get; }
+
+    }
+}
